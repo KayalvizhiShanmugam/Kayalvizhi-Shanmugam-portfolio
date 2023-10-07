398e46868ec0da1b87f5021dba0cacc9257411aa
KayalvizhiShanmugam <s.kayalvizhi00@gmail.com>
2023-10-07 14:51:25 +0530

    react root

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
